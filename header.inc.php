0394639771e3f31e393cf9a67f7c55aa3e039d76
rimuzakki <rimuzakki@gmail.com>
2017-09-08 12:42:43 +0700

    header logo tengah
    iki ngene sek ya wkwk, mengko bengi jajal sekalian tak gawene hehehe

---
+++ header.inc.php
@@ -0,0 +1,71 @@
+<?php session_start();?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<link rel="icon" type="image/png" href="assets/img/favicon.ico">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+	<title>AssesmentSample</title>
+
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+	<meta name="viewport" content="width=device-width" />
+
+	
+	<!-- Bootstrap core CSS -->
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+
+	<!--  Light Bootstrap Table core CSS    -->
+	<link href="assets/css/light-bootstrap-dashboard.css" rel="stylesheet"/>
+
+	<!--     Fonts and icons     -->
+	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+	<link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
+
+	<style type="text/css">
+		.navbar {
+			background-color: orange;
+		}
+		.navbar-right	{
+			margin-right: 0px; 
+		}
+		.navbar-left, .navbar-right {
+			margin-top: 5px;
+		}
+		.navbar-brand {
+		  transform: translateX(-50%);
+		  left: 50%;
+		  position: absolute;
+		}
+
+
+		/* DEMO example styles for logo image */
+		.navbar-brand {
+		  padding: 0px !important;
+		}
+		.navbar-brand>img {
+		  height: 100%;
+		  width: auto;
+		  padding: 4px 0px;
+		}
+	</style>
+</head>
+<body>
+
+<!-- <div class="wrapper"> -->
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#"><img src="assets/img/MPM_Logo Group_White.png"></a>
+    </div>
+
+    <div class="navbar-left">
+    	<h5><strong>Assesment</strong>Sample</h5>
+    </div>
+    <div class="navbar-right">
+    	<h5>timer</h5>
+    </div>
+  </div><!-- /.container-fluid -->
+</nav>
